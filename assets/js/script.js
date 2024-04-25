99241660ecd5e82e3723f7fe883d498473b7e5cf
arcadejk Kakpo <arcadej@hotmail.fr>
2024-04-25 10:10:35 +0000

    Create play, pause and shuffle variables.

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const playlistMovies = document.getElementById("playlist-movies");
+const playButton = document.getElementById("play");
+const pauseButton = document.getElementById("pause");
+const shuffleButton = document.getElementById("shuffle");
